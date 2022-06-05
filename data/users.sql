953ead05c3b55116bb5fe1e2db0b00730141b2ad
Savin Sergey <razi@yandex.ru>
2022-06-05 14:54:01 +0300

    Обновил дамп таблицы users

--- data/users.sql
+++ data/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 29 2022 г., 22:40
+-- Время создания: Июн 05 2022 г., 14:53
 -- Версия сервера: 10.6.7-MariaDB-log
 -- Версия PHP: 7.4.29
 
@@ -32,12 +32,20 @@ CREATE TABLE `users` (
   `surname` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `patronymic` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(270) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_change` timestamp NOT NULL,
   `isadmin` int(1) NOT NULL DEFAULT 0,
   `login` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Таблица пользователей';
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `surname`, `name`, `patronymic`, `password`, `user_change`, `isadmin`, `login`, `email`) VALUES
+(1, 'Петров', 'Семен', 'Викторович', '$6$52072348ecfb3704$V1pL.MiRl3fIPu73ZTG4Qyvck.MAhB0xb4AgAgy4G3x6c1hWKG6ZJbKVG3ZCdR9PvA.jREwunoM8t2qg/8umu/', '2022-06-05 11:47:46', 0, 'psv1234', 'sss@gmail.com'),
+(2, 'Иванов', 'Иван', 'Петрович', '$6$9c3cccef5da16431$dAUce.VFlR/pVm1rpOTbYoMU.LToo9FOOTgaKhwXFV8Oec3UWxSeDO8vFq2ALUEz0VC5b5yIPeT56xqHmLkhy/', '2022-06-05 11:49:49', 0, 'ivan1991', 'razi@yandex.ru');
 
 --
 -- Индексы сохранённых таблиц
@@ -47,7 +55,8 @@ CREATE TABLE `users` (
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -57,7 +66,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
